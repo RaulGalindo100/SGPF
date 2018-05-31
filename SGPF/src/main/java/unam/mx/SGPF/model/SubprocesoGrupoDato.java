0deb694954bd79205f3440e168707d3e91765172
Miguel Angel Piña Avelino <miguel_pinia@ciencias.unam.mx>
2018-05-31 15:22:16 -0500

    Elimina comentario con mi nombre.

--- SGPF/src/main/java/unam/mx/SGPF/model/SubprocesoGrupoDato.java
+++ SGPF/src/main/java/unam/mx/SGPF/model/SubprocesoGrupoDato.java
@@ -19,10 +19,6 @@ import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 import javax.xml.bind.annotation.XmlRootElement;
 
-/**
- *
- * @author miguel
- */
 @Entity
 @Table(name = "subprocesoGrupoDato", catalog = "SGPF", schema = "")
 @XmlRootElement
